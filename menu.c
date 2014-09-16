a9174d10bf99bb4c7ae8ea497db6450dfb7644bf
qiong1130 <1538877085@qq.com>
2014-09-16 18:20:14 +0800

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,19 @@
+/**************************************************************************************************/
+/* Copyright (C) hanqiong.com, USTC, 2014-2015                                                    */
+/*                                                                                                */
+/*  FILE NAME             :  menu.c                                                               */
+/*  PRINCIPAL AUTHOR      :  Hanqiong                                                             */
+/*  SUBSYSTEM NAME        :  menu                                                                 */
+/*  MODULE NAME           :  menu                                                                 */
+/*  LANGUAGE              :  C                                                                    */
+/*  TARGET ENVIRONMENT    :  ANY                                                                  */
+/*  DATE OF FIRST RELEASE :  2014/09/14                                                           */
+/*  DESCRIPTION           :  This is a menu program                                               */
+/**************************************************************************************************/
+
+/*
+ * Revision log :
+ *
+ * Created by Hanqiong, 2014/09/14
+ *
+*/
